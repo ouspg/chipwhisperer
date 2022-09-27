894584dacd89f520f468986c7b3acd311367afc9
joniumGit <52005121+joniumGit@users.noreply.github.com>
2022-09-27 14:57:07 +0300

    remove jupyter password

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,4 +1,4 @@
 c.NotebookApp.allow_password_change = True
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.open_browser = False
-c.NotebookApp.password_required = True
+c.NotebookApp.password_required = False
